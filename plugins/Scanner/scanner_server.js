d2d165fa6fa87d431adfcad164c99d2977e579ab
Highpoint2000 <168109804+Highpoint2000@users.noreply.github.com>
2024-08-16 20:12:36 +0200

    Update scanner_server.js

--- plugins/Scanner/scanner_server.js
+++ plugins/Scanner/scanner_server.js
@@ -1,8 +1,8 @@
 ///////////////////////////////////////////////////////////////
 ///                                                         ///
-///  SCANNER SERVER SCRIPT FOR FM-DX-WEBSERVER (V2.0 BETA)  /// 
+///  SCANNER SERVER SCRIPT FOR FM-DX-WEBSERVER (V2.0)       /// 
 ///                                                         ///
-///                                  last update: 16.08.24  ///
+///                             last update: 16.08.24       ///
 ///                                                         /// 
 ///  by Highpoint                                           ///
 ///  powered by PE5PVB                                      ///     
@@ -14,9 +14,9 @@
 ///  This plugin only works from web server version 1.2.3!!!
 
 const Autoscan_PE5PVB_Mode = false; // Set to "true" if ESP32 with PE5PVB firmware is being used and you want to use the auto scan mode of the firmware
-const Search_PE5PVB_Mode = false; // Set to "true" if ESP32 with PE5PVB firmware is being used and you want to use the search mode of the firmware
-const StartAutoScan = 'auto'; // Set to "off/on/auto" (on - starts with webserver, auto - starts scanning after 10 s when no user is connected)
-const AntennaSwitch = 'off';  // Set to "off/on" for automatic switching with more than 1 antenna at the upper band limit
+const Search_PE5PVB_Mode = true; // Set to "true" if ESP32 with PE5PVB firmware is being used and you want to use the search mode of the firmware
+const StartAutoScan = 'off'; // Set to "off/on/auto" (on - starts with webserver, auto - starts scanning after 10 s when no user is connected)
+const AntennaSwitch = 'on';  // Set to "off/on" for automatic switching with more than 1 antenna at the upper band limit
 
 let defaultSensitivityValue = 25; // Value in dBf/dBµV: 5,10,15,20,25,30,35,40,45,50,55,60 | in dBm: -115,-110,-105,-100,-95,-90,-85,-80,-75,-70,-65,-60
 let defaultScanHoldTime = 7; // Value in s: 1,3,5,7,1,15,20,30 
@@ -26,7 +26,7 @@ let defaultScannerMode = 'normal'; // Set the startmode: normal, blacklist, or w
 
 //////////////////////////////////////////////////////////////////////////////////////
 
-const pluginVersion = 'V2.0 BETA'; 
+const pluginVersion = 'V2.0'; 
 
 const WebSocket = require('ws');
 const fs = require('fs');
