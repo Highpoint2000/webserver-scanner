8c0611d97734232cccff57b49bfc2c89210d62fd
Highpoint2000 <168109804+Highpoint2000@users.noreply.github.com>
2024-09-04 13:22:45 +0200

    Update ScannerPlugin.js

--- plugins/ScannerPlugin.js
+++ plugins/ScannerPlugin.js
@@ -1,7 +1,7 @@
 // Plugin configuration, this is used in the administration when plugins are loaded
 var pluginConfig = {
     name: 'Scanner Plugin',
-    version: '2.2a BETA',
+    version: '2.3',
     author: 'Highpoint',
     frontEndPath: 'Scanner/scanner.js'
 }
