18d3a214b99c5a0d536e283e900986d31ba2c616
Highpoint2000 <168109804+Highpoint2000@users.noreply.github.com>
2025-04-22 11:46:27 +0200

    Update scanner.js

--- plugins/Scanner/scanner.js
+++ plugins/Scanner/scanner.js
@@ -1,9 +1,9 @@
 (() => {
 ///////////////////////////////////////////////////////////////
 ///                                                         ///
-///  SCANNER CLIENT SCRIPT FOR FM-DX-WEBSERVER (V3.6)       ///
+///  SCANNER CLIENT SCRIPT FOR FM-DX-WEBSERVER (V3.6a)      ///
 ///                                                         ///
-///  by Highpoint               last update: 21.04.25       ///
+///  by Highpoint               last update: 22.04.25       ///
 ///  powered by PE5PVB                                      ///
 ///                                                         ///
 ///  https://github.com/Highpoint2000/webserver-scanner     ///
@@ -14,7 +14,7 @@
 	
 ///////////////////////////////////////////////////////////////
 
-    const plugin_version = '3.6'; // Plugin version
+    const plugin_version = '3.6a'; // Plugin version
 	const plugin_path = 'https://raw.githubusercontent.com/Highpoint2000/webserver-scanner/';
 	const plugin_JSfile = 'refs/heads/main/plugins/Scanner/scanner.js'
 	const plugin_name = 'Scanner';
