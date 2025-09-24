a3154d0c1ba26d5fecf94ac015581cffdb4422cc
Highpoint2000 <168109804+Highpoint2000@users.noreply.github.com>
2025-09-24 08:47:07 +0200

    Update scanner.js

--- plugins/Scanner/scanner.js
+++ plugins/Scanner/scanner.js
@@ -17,7 +17,7 @@
 	
 ///////////////////////////////////////////////////////////////
 
-	const pluginVersion = '3.8d';
+	const pluginVersion = '3.8e';
 	const pluginName = "Scanner";
 	const pluginHomepageUrl = "https://github.com/Highpoint2000/webserver-scanner/releases";
 	const pluginUpdateUrl = "https://raw.githubusercontent.com/Highpoint2000/webserver-scanner/refs/heads/main/plugins/Scanner/scanner.js";
